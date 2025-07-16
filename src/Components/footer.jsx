2e61faa8eb0a7a520259e9d7146c7c51c920239f
Lokkuchakreshkumar <lokkuchakreshkumar@gmail.com>
2025-07-16 16:01:53 +0530

    added footer animation

--- src/Components/footer.jsx
+++ src/Components/footer.jsx
@@ -1,5 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHeart } from '@fortawesome/free-solid-svg-icons' // this is RIGHT
+import { motion } from "motion/react"
 
 
 
@@ -7,7 +8,17 @@ export default function Footer(){
     return(
         <div className="mt-8 sm:mt-16 flex flex-col sm:text-2xl text-lg   mb-8 items-center  text-white">
             <div>
-               made with <FontAwesomeIcon icon={faHeart} className='text-cyan-500 drop-shadow-[0px_0px_15px_cyan] mx-2' /> by chakresh
+               made with <motion.div
+               animate={{
+                scale:[1,1.2,1]
+               }}
+               transition={{
+                repeat:Infinity,
+                duration:5
+                
+               }}
+               className='inline-block'
+               ><FontAwesomeIcon icon={faHeart} className='text-cyan-500 drop-shadow-[0px_0px_15px_cyan] mx-2' /></motion.div> by chakresh
             </div>
             <div className='flex flex-col items-center'>
                 &copy;2025 All rights reserved<div className='text-sm'>(if you use my template, a dm makes me happy)</div>
