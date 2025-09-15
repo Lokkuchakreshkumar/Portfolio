b1fc88e73598199d11cdd3c7798521b560e805ad
Lokkuchakreshkumar <lokkuchakreshkumar@gmail.com>
2025-09-15 21:39:53 +0530

    added framer motion

--- src/Components/Newfooter.jsx
+++ src/Components/Newfooter.jsx
@@ -1,26 +1,20 @@
-
 import { FaHeart } from 'react-icons/fa'
 
 const Newfooter = () => {
   return (
-         <div className="mt-8 sm:mt-16 flex flex-col sm:text-2xl text-lg   mb-8 items-center  text-white">
-            <div>
-               made with <motion.div
-               animate={{
-                scale:[1,1.2,1]
-               }}
-               transition={{
-                repeat:Infinity,
-                duration:3
-                
-               }}
-               className='inline-block'
-               ><FontAwesomeIcon icon={FaHeart} className='text-cyan-500 drop-shadow-[0px_0px_15px_cyan] mx-2' /></motion.div> by chakresh
-            </div>
-            <div className='flex flex-col items-center'>
-                &copy;2025 All rights reserved<div className='text-sm'>(if you use my template, a dm makes me happy)</div>
-            </div>
-        </div>
+    <div className="mt-8 sm:mt-16 flex flex-col sm:text-2xl text-lg mb-8 items-center text-white">
+      <div>
+        made with 
+        <span className='inline-block'>
+          <FaHeart className='text-cyan-500 drop-shadow-[0px_0px_15px_cyan] mx-2 inline-block' />
+        </span> 
+        by chakresh
+      </div>
+      <div className='flex flex-col items-center'>
+        &copy;2025 All rights reserved
+        <div className='text-sm'>(if you use my template, a dm makes me happy)</div>
+      </div>
+    </div>
   )
 }
 
